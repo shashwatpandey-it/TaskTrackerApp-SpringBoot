2248b0e0d8d2de1c9e338f232b64482bdd66b6b6
Shashwat Pandey <2001shashwat.rdr@gmail.com>
2023-02-27 22:32:16 +0530

    initial commit

---
+++ src/main/java/com/tasktrackerapplication/TaskTrackerApplication.java
@@ -0,0 +1,15 @@
+package com.tasktrackerapplication;
+
+import java.io.Console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApplication {
+
+	public static void main(String[] args) {		
+		SpringApplication.run(TaskTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasktrackerapplication/TaskTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasktrackerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
